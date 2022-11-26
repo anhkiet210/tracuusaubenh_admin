f0cc717c51b0c2927e4a28e4dbbf38ea2d2b347c
anhkiet210 <anhkietk053@gmail.com>
2022-11-26 23:57:23 +0700

    change somethings in file _document.js and PageChange.jsx

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#000000" />
           <link
             rel="shortcut icon"
-            href="https://res.cloudinary.com/ak-tracuusaubenh/image/upload/v1667751701/ak-tracuusaubenh/Screenshot_2022-09-02_152018-removebg-preview_1_meicoh.png"
+            href="https://res.cloudinary.com/ak210/image/upload/v1667751701/ak-tracuusaubenh/Screenshot_2022-09-02_152018-removebg-preview_1_meicoh.png"
           />
         </Head>
         <body className="text-slate-700 antialiased">
